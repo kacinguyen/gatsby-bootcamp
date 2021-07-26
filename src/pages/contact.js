5269f5f32967bdb52b3f53a8947187ee7cbde24c
Kaci Nguyen <kacinguyen@utexas.edu>
2021-07-26 07:17:19 -0500

    Create shared header and footer component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import Footer from './components/footer'
+import Header from './components/header'
+
 const ContactPage = () => {
     return (
         <div>
+            <Header/>
             <h1>Contact</h1>
             <p>Reach me via <a href="https://kacinguyen.com" target="_blank" >kaci</a></p>
-
+        < Footer />
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 // React component that links between pages
 // Pre-loads content
 import { Link } from 'gatsby'
+import Footer from './components/footer'
+import Header from './components/header'
 
 const IndexPage = () => {
   return (
     <div>
+      <Header />
       <h1>Hello!</h1>
       <h2>I'm Kaci, a tea connoisseur :-)</h2>
       <p><Link to="/about">Learn more</Link></p>
+      <Footer />
+    
     </div>
   )
 }
