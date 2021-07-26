a4df6bf02fcf8d29a5ebb56a36b958456eb7b4d8
Kaci Nguyen <kacinguyen@utexas.edu>
2021-07-26 07:58:05 -0500

    Add gatby plugin sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 // Pre-loads content
 import { Link } from 'gatsby'
 
-import Layout from './components/layout'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
