a8b9334d0c26ae8be0c238f17070bdd452d167cc
Kaci Nguyen <kacinguyen@utexas.edu>
2021-07-26 07:00:15 -0500

    Create pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import { divide } from "lodash";
+import React from "react";
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Posts will show up here</p>
+        </div>
+    )
+}
+
+export default AboutPage
