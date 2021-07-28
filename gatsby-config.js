d1c3baa39c54d88146fde27fb3afb18464149cd4
Kaci Nguyen <kacinguyen@utexas.edu>
2021-07-27 22:15:15 -0500

    Add contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,15 @@ module.exports = {
   },
 
   plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
+
+
     'gatsby-plugin-sass',
     // loads raw files from file system
     {
