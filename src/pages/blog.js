9da1a9b9be51dba4e7b5dcdecb5774f29e044ed4
Kaci Nguyen <kacinguyen@utexas.edu>
2021-08-02 21:57:50 -0500

    Add description to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -31,6 +31,7 @@ const BlogPage = () => {
         // render with JSX 
         <Layout>
             <h1>Blog</h1>
+            <p>Posts are sourced from the file system and are dynamically generated.</p>
             <ol className={blogStyles.posts}>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
